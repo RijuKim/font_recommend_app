47ba0d9b9ebe0de30d8fa6ba32d1300014b217c2
RijuKim <rijoo1121@gmail.com>
2023-08-15 17:02:22 +0900

    Update: index.js 부모 컴포넌트 수정

--- font_recommend_app/src/index.js
+++ font_recommend_app/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Cards from './components/Card';
 import Header from './components/Header';
+import Tab from './components/Tab';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     < Header />
-    <Cards />
+    <Tab/>
   </React.StrictMode>
 );
 
