37f392f5d4db40a7950161fdf6b4aac91c6af1ec
jykim0925 <mslkd0925@gmail.com>
2023-09-03 16:14:14 +0900

    12롯데마트행복, HY얕은샘물M 폰트 css 수정

--- font_recommend_app/src/components/ResultItem.jsx
+++ font_recommend_app/src/components/ResultItem.jsx
@@ -5,8 +5,11 @@ export default function ResultItem(props) {
 
     // 만약 fontName이 숫자로 시작하는 경우에 _를 붙여 fontFamily를 지정
     const modifiedFontName = /^[0-9]/.test(fontName) ? `_${fontName}` : fontName;
+   
     // 만약 fontName이 공백 + 숫자로 끝나는 경우에 공백과 숫자를 제거한 fontFamily를 사용
     const trimmedFontName = modifiedFontName.replace(/ (\d+(\.\d+)?)$/, '');
+
+
 
     return (
         <div className='result-item-wrapper'>
