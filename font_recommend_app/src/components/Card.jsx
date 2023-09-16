1a3c882e79be3b5e349ab97572bcca37e88eb305
jykim0925 <mslkd0925@gmail.com>
2023-09-17 01:10:50 +0900

    두 번째 줄 폰트 카테고리 삭제

--- font_recommend_app/src/components/Card.jsx
+++ font_recommend_app/src/components/Card.jsx
@@ -9,11 +9,11 @@ const initialCardData = [
     { category: '민부리체', titlefont: '맑은 고딕', children: '맑은 고딕', children2: '나눔스퀘어', children3: '타이포 씨고딕180', children4: '휴먼고딕', children5: '유토이미지 고딕 R', children6: '헬스셋고딕Bold', children7: 'DX모던고딕 RoundBold', children8: 'DX단선고딕 Thin' },
     { category: '부리체', titlefont: 'KBIZ 한마음명조 M', children: 'KBIZ 한마음명조 M', children2: '나눔명조', children3: '제주명조', children4: 'HY견명조', children5: 'DX신문명조', children6: '빛의계승자체 Regular', children7: '조선일보명조', children8: '함초롬바탕체' },
     { category: '손글씨', titlefont: '나눔손글씨 다행체', children: '나눔손글씨 다행체', children2: '나눔손글씨 반짝반짝 별', children3: '어비 나현체', children4: '어비 소윤체', children5: '온글잎 안될과학약체', children6: '나눔손글씨 중학생', children7: '나눔손글씨 펜', children8: '나눔손글씨 한윤체' },
-    { category: '장식체', titlefont: 'HS겨울눈꽃체', children: 'HS겨울눈꽃체', children2: 'HS두꺼비체', children3: '가비아 봄바람체', children4: '창원단감아삭체 Bold', children5: '가나초콜릿체', children6: 'HanS 붐붐', children7: '116수박화체', children8: 'DX동화나라 Bold' },
-    { category: '픽셀체', titlefont: '픽셀체', children: '116앵무부리', children2: ' 나눔굴림', children3: '나눔고딕', children4: '나눔고딕', children5: '나눔고딕', children6: '나눔고딕', children7: '나눔고딕', children8: '나눔고딕' },
-    { category: '고전체', titlefont: '고전체', children: '산스체', children2: ' 나눔고딕', children3: '나눔고딕', children4: '116watermelon', children5: '나눔고딕', children6: '나눔고딕', children7: '나눔고딕', children8: '한겨레결체' },
-    { category: '탈네모', titlefont: '탈네모', children: '명조체', children2: ' 나눔명조', children3: '나눔고딕', children4: '나눔고딕', children5: '나눔고딕', children6: '나눔고딕', children7: '나눔고딕', children8: '나눔고딕' },
-    { category: '캘리폰트', titlefont: '캘리폰트', children: '수박화체', children2: '조선', children3: '12롯데마트드림Light', children4: '수박수박', children5: '116앵무부리', children6: '나눔고딕', children7: '나눔고딕', children8: '헬스' }
+    { category: '장식체', titlefont: 'HS겨울눈꽃체', children: 'HS겨울눈꽃체', children2: 'HS두꺼비체', children3: '가비아 봄바람체', children4: '창원단감아삭체 Bold', children5: '가나초콜릿체', children6: 'HanS 붐붐', children7: '116수박화체', children8: 'DX동화나라 Bold' }
+    // { category: '픽셀체', titlefont: '픽셀체', children: '116앵무부리', children2: ' 나눔굴림', children3: '나눔고딕', children4: '나눔고딕', children5: '나눔고딕', children6: '나눔고딕', children7: '나눔고딕', children8: '나눔고딕' },
+    // { category: '고전체', titlefont: '고전체', children: '산스체', children2: ' 나눔고딕', children3: '나눔고딕', children4: '116watermelon', children5: '나눔고딕', children6: '나눔고딕', children7: '나눔고딕', children8: '한겨레결체' },
+    // { category: '탈네모', titlefont: '탈네모', children: '명조체', children2: ' 나눔명조', children3: '나눔고딕', children4: '나눔고딕', children5: '나눔고딕', children6: '나눔고딕', children7: '나눔고딕', children8: '나눔고딕' },
+    // { category: '캘리폰트', titlefont: '캘리폰트', children: '수박화체', children2: '조선', children3: '12롯데마트드림Light', children4: '수박수박', children5: '116앵무부리', children6: '나눔고딕', children7: '나눔고딕', children8: '헬스' }
 ];
 
 function Cards(props) {
@@ -81,9 +81,9 @@ function Cards(props) {
         )
     );
 
-    // 카드 데이터를 두 개로 분할하여 렌더링
-    const firstHalf = filteredCardData.slice(0, Math.ceil(filteredCardData.length / 2));
-    const secondHalf = filteredCardData.slice(Math.ceil(filteredCardData.length / 2));
+    // 카드 데이터를 두 개로 분할하여 렌더링 -> 한 줄로 렌더링(수정)
+    const firstHalf = filteredCardData.slice(0, Math.ceil(filteredCardData.length));
+    // const secondHalf = filteredCardData.slice(Math.ceil(filteredCardData.length / 2));
 
     return (
         <div className='body'>
